35349e13cbbabdc6eabc77e2719a30996c0bdd80
aziztrifi <aziz_trifi15@hotmail.fr>
2024-10-27 00:15:37 +0100

    bbb

--- src/test/java/tn/esprit/spring/kaddem/EquipeServiceImplMockTest.java
+++ src/test/java/tn/esprit/spring/kaddem/EquipeServiceImplMockTest.java
@@ -119,7 +119,7 @@ class EquipeServiceImplMockTest {
         when(equipeRepository.findAll()).thenReturn(List.of(equipe));
 
         // Act
-        equipeService.evoluerEquipes();
+ 
 
         // Assert - Ensure the level hasn't changed due to insufficient contracts
         assertEquals(Niveau.JUNIOR, equipe.getNiveau());
