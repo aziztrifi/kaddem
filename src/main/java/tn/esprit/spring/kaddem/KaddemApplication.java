2d2b6b72c4b4ac5704cfcfdb4d081e9fc247537b
aziztrifi <aziz_trifi15@hotmail.fr>
2024-10-27 02:02:51 +0100

    neww

--- src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
+++ src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
-@SpringBootApplication
+
+
+@EnableScheduling // Enables scheduling capabilities in the application
+@SpringBootApplication // Indicates this is a Spring Boot application
 public class KaddemApplication {
 
     public static void main(String[] args) {
+        // Launches the Spring Boot application
         SpringApplication.run(KaddemApplication.class, args);
     }
-
 }
+
