b440cfe535d2a8f56f8677f32685ab2713d6273a
aziztrifi <aziz_trifi15@hotmail.fr>
2024-10-31 23:12:48 +0100

    scscs

--- src/main/java/tn/esprit/spring/kaddem/entities/Equipe.java
+++ src/main/java/tn/esprit/spring/kaddem/entities/Equipe.java
@@ -27,9 +27,7 @@ public class Equipe implements Serializable{
 
     }
 
-    public Equipe(String nomEquipe) {
-        this.nomEquipe = nomEquipe;
-    }
+
 
     public Equipe(String nomEquipe, Niveau niveau) {
         super();
@@ -44,20 +42,7 @@ public class Equipe implements Serializable{
         this.niveau = niveau;
     }
 
-    public Equipe(String nomEquipe, Niveau niveau, Set<Etudiant> etudiants, DetailEquipe detailEquipe) {
-        this.nomEquipe = nomEquipe;
-        this.niveau = niveau;
-        this.etudiants = etudiants;
-        this.detailEquipe = detailEquipe;
-    }
 
-    public Equipe(Integer idEquipe, String nomEquipe, Niveau niveau, Set<Etudiant> etudiants, DetailEquipe detailEquipe) {
-        this.idEquipe = idEquipe;
-        this.nomEquipe = nomEquipe;
-        this.niveau = niveau;
-        this.etudiants = etudiants;
-        this.detailEquipe = detailEquipe;
-    }
 
     public Set<Etudiant> getEtudiants() {
         return etudiants;
@@ -67,20 +52,11 @@ public class Equipe implements Serializable{
         this.etudiants = etudiants;
     }
 
-    public DetailEquipe getDetailEquipe() {
-        return detailEquipe;
-    }
-
-    public void setDetailEquipe(DetailEquipe detailEquipe) {
-        this.detailEquipe = detailEquipe;
-    }
 
     public Integer getIdEquipe() {
         return idEquipe;
     }
-    public void setIdEquipe(Integer idEquipe) {
-        this.idEquipe = idEquipe;
-    }
+
     public String getNomEquipe() {
         return nomEquipe;
     }
